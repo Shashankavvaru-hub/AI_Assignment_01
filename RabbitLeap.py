9d92ac41a27c8432d00b3becf06efe105dcf8723
Shashank Avvaru <alvshashank@gmail.com>
2025-07-25 15:26:49 +0530

    Update RabbitLeap.py

--- RabbitLeap.py
+++ RabbitLeap.py
@@ -1,40 +1,36 @@
 class State:
-    def __init__(self, config):
-        self.config = config
-        self.goal = ['R', 'R', 'R', '_', 'L', 'L', 'L']
+    def __init__(self,config):
+        self.config=config
+        self.goal=['R', 'R', 'R', '_', 'L', 'L', 'L']
 
     def goalTest(self):
-        return self.config == self.goal
+        return self.config==self.goal
 
     def moveGen(self):
-        children = []
+        children=[]
         for i in range(len(self.config)):
-            if self.config[i] == 'L':
-                # Move right
+            if self.config[i]=='L':
                 if i + 1 < len(self.config) and self.config[i + 1] == '_':
-                    next_state = self.config[:]
-                    next_state[i], next_state[i + 1] = next_state[i + 1], next_state[i]
+                    next_state=self.config[:]
+                    next_state[i], next_state[i + 1]=next_state[i + 1], next_state[i]
                     children.append(State(next_state))
-                # Jump over R
                 if i + 2 < len(self.config) and self.config[i + 1] == 'R' and self.config[i + 2] == '_':
-                    next_state = self.config[:]
+                    next_state=self.config[:]
                     next_state[i], next_state[i + 2] = next_state[i + 2], next_state[i]
                     children.append(State(next_state))
-            elif self.config[i] == 'R':
-                # Move left
-                if i - 1 >= 0 and self.config[i - 1] == '_':
-                    next_state = self.config[:]
-                    next_state[i], next_state[i - 1] = next_state[i - 1], next_state[i]
+            elif self.config[i]=='R':
+                if i-1 >= 0 and self.config[i-1]=='_':
+                    next_state=self.config[:]
+                    next_state[i], next_state[i-1]=next_state[i-1],next_state[i]
                     children.append(State(next_state))
-                # Jump over L
-                if i - 2 >= 0 and self.config[i - 1] == 'L' and self.config[i - 2] == '_':
-                    next_state = self.config[:]
-                    next_state[i], next_state[i - 2] = next_state[i - 2], next_state[i]
+                if i-2 >= 0 and self.config[i-1] == 'L' and self.config[i-2]=='_':
+                    next_state=self.config[:]
+                    next_state[i], next_state[i-2]=next_state[i-2],next_state[i]
                     children.append(State(next_state))
         return children
 
     def __eq__(self, other):
-        return isinstance(other, State) and self.config == other.config
+        return isinstance(other, State) and self.config==other.config
 
     def __hash__(self):
         return hash(tuple(self.config))
@@ -43,72 +39,71 @@ class State:
         return ''.join(self.config)
 
 def removeVisited(children, OPEN, CLOSED):
-    open_nodes = []
-    for node, _ in OPEN:
+    open_nodes=[]
+    for node,_ in OPEN:
         open_nodes.append(node)
-    closed_nodes = []
+    closed_nodes=[]
     for node, _ in CLOSED:
         closed_nodes.append(node)
-    new_nodes = []
+    new_nodes=[]
     for node in children:
         if node not in open_nodes and node not in closed_nodes:
             new_nodes.append(node)
     return new_nodes
 
 def reconstructPath(node_pair, CLOSED):
-    parent_map = {}
-    for node, parent in CLOSED:
-        parent_map[node] = parent
-    node, parent = node_pair
-    path = [node]
+    parent_map={}
+    for node,parent in CLOSED:
+        parent_map[node]=parent
+    node, parent=node_pair
+    path=[node]
     while parent is not None:
         path.append(parent)
-        parent = parent_map[parent]
+        parent=parent_map[parent]
     path.reverse()
     for i in range(len(path)):
         print(path[i])
-        if i != len(path)-1:
+        if i!=len(path)-1:
             print(" <- ")
     return path
 
 def bfs(start):
-    OPEN = [(start, None)]
-    CLOSED = []
+    OPEN=[(start, None)]
+    CLOSED=[]
     while OPEN:
-        node_pair = OPEN.pop(0)
-        node, parent = node_pair
+        node_pair=OPEN.pop(0)
+        node, parent=node_pair
         if node.goalTest():
             print("Goal is found")
             reconstructPath(node_pair, CLOSED)
             return
         CLOSED.append(node_pair)
-        children = node.moveGen()
-        new_nodes = removeVisited(children, OPEN, CLOSED)
-        new_pairs = []
+        children=node.moveGen()
+        new_nodes=removeVisited(children, OPEN, CLOSED)
+        new_pairs=[]
         for child in new_nodes:
             new_pairs.append((child, node))
-        OPEN += new_pairs
+        OPEN+=new_pairs
 
 def dfs(start):
-    OPEN = [(start, None)]
-    CLOSED = []
+    OPEN=[(start, None)]
+    CLOSED=[]
     while OPEN:
-        node_pair = OPEN.pop(0)
-        node, parent = node_pair
+        node_pair=OPEN.pop(0)
+        node,parent=node_pair
         if node.goalTest():
             print("Goal is found")
             reconstructPath(node_pair, CLOSED)
             return
         CLOSED.append(node_pair)
-        children = node.moveGen()
-        new_nodes = removeVisited(children, OPEN, CLOSED)
-        new_pairs = []
+        children=node.moveGen()
+        new_nodes=removeVisited(children, OPEN, CLOSED)
+        new_pairs=[]
         for child in new_nodes:
             new_pairs.append((child, node))
-        OPEN = new_pairs + OPEN
+        OPEN=new_pairs+OPEN
 
-# Example usage:
-init_state = State(['L', 'L', 'L', '_', 'R', 'R', 'R'])
+init_state=State(['L', 'L', 'L', '_', 'R', 'R', 'R'])
 print("BFS solution:")
 bfs(init_state)
 print("\nDFS solution:")
